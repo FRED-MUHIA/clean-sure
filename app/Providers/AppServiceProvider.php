fa4b692493e9034fd2c96a6692bc342eacebe4f7
Your Name <you@example.com>
2026-03-25 20:29:36 +0300

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->forceFileBackends()) {
+            config([
+                'session.driver' => 'file',
+                'cache.default' => 'file',
+                'queue.default' => 'sync',
+            ]);
+        }
     }
 
     /**
@@ -20,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    }
+
+    private function forceFileBackends(): bool
+    {
+        return filter_var(env('FORCE_FILE_BACKENDS', true), FILTER_VALIDATE_BOOL);
     }
 }
